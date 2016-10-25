f21a904c1f7131595fbc24fe21f31fd39a1ce364
Chandra <chandra.idev@gmail.com>
2016-10-25 14:47:56 -0700

    Checking with Level 1 deeper

--- Level/QR/QR.h
+++ Level/QR/QR.h
@@ -13,7 +13,7 @@
 FOUNDATION_EXPORT double QRVersionNumber;
 
 //! Project version string for QR.
-FOUNDATION_EXPORT const unsigned char QRVersionString[];
+FOUNDATION_EXPORT const unsigned char QRVersionString[];/Users/cpoleped/Projects/Visa/Samples/Carthage/CartageSample/Cartfile.resolved
 
 // In this header, you should import all the public headers of your framework using statements like #import <QR/PublicHeader.h>
 
